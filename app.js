bbe6ca41b8bbc4faea2b81b3ec3e71b10e3bef33
Mesum Abbas <syedmesum387@gmail.com>
2021-12-09 12:04:20 +0500

    First Git

---
+++ app.js
@@ -0,0 +1,137 @@
+//ch#1
+//assignment 1
+//alert("Hi Welcome To MY Website");
+
+
+//asssignment 2
+//alert("Error! Please Enter a valid password.");
+
+
+//assignment 3
+//alert("Welcome To JS Land..\n Happy Coding!");
+
+
+
+//assignment 4
+//alert("Welcome To JS Land..");
+//alert("Happy Coding!");
+
+
+//.
+
+
+
+//assignment 5
+//console.log("Hello....I can run JS through my web browser's console.");
+
+
+
+
+// Chapter 2
+
+//assignment 1
+// var username
+
+//assignment 2
+// var myname = "Syed Mesum Abbas Rizvi";
+// alert(myname);
+
+
+//assignment 3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+
+//assigment 4
+// var name = "John Doe";
+// var age = "15 years old";
+// var occupation = "Certified Mobile Application Developer.";
+// alert(name);
+// alert(age);
+// alert(occupation);
+
+
+
+//assignment 5
+// var allelements = ("Pizza\nPizz\nPiz\nPi\nP");
+// alert(allelements);
+
+
+//assignment 6
+// var email = ("example@example.com");
+// var email2 = ("My Email Address Is ");
+// alert(email2+email);
+
+//assignment 7
+// var book = "A Smarter Way To Learn JavaScript.";
+// var text = "I am trying learn from the book ";
+// alert(text+book);
+
+
+//assignment 8
+// document.write("Yah! i can write HTML content through JavaScript");
+
+
+
+//assignment 9
+// var content =  ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert(content);
+
+
+
+//Chapter 3
+//Assignment 1
+// var age;
+// age = "I am 15 years old";
+// alert(age);
+
+// //Assignment 2
+// var visited = "1";
+// alert("you have visited this site " + visited + " times");
+
+
+//Assignment 3
+// var birthyear;
+// birthyear = "2006";
+// document.write("My Birth year is " + birthyear + ".");
+
+
+//Assignment 4
+// var visitorname = prompt("Enter Your Name");
+// var producttitle = prompt("Product Name");
+// var quantity = +prompt("Product Quantity");
+// document.write(visitorname + " Ordered " + quantity + " " + producttitle);
+
+
+
+//Chapter 4
+//Assignment 1
+// var variable1 = "sdfhj",
+//     variable2 = "dfhsd",
+//     variable3 = "alsfj";
+
+
+
+//Assignment 2
+//Legal Variables
+// var mesum;
+// var _mesum;
+// var $mesum;
+// var sdjfh;
+// var asfjksd;
+
+
+// //Illegal Names
+// var 1meusm;
+// var 'meusm';
+// var mesun-1;
+// var meusm abbas ;
+// var meusm-abbas;
+
+
+
+//Assignment 3
+
+
+
